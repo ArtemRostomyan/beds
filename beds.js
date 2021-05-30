3234f89bcfd1539dccdd7363180c91c93f5bb117
ArtemRostomyan <71639600+ArtemRostomyan@users.noreply.github.com>
2021-05-30 15:07:09 +0500

    Update beds.js

--- beds.js
+++ beds.js
@@ -275,7 +275,7 @@
         "cloth": "mat-1",
         "foam": "ультра soft",
         "DEFAULT_cloth": "mat-1",
-        "legs": "металлическая2",
+        "children_legs_type": "leg-2",
         "dop_info": {
             "visota_spalnogo_mesta": 62,
             "visota_chelikom": 100,
